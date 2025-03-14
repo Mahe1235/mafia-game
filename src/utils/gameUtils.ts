41590529ca37d73155e391dbedcffffa28c2c4f4
Mahe1235 <mahendrab1094@gmail.com>
2025-03-14 19:04:37 +0530

    Add missing dependencies and create gameUtils.ts to fix build errors

---
+++ src/utils/gameUtils.ts
@@ -0,0 +1,9 @@
+/**
+ * Game utility functions
+ * Re-exports from gameLogic.ts for backward compatibility
+ */
+
+import { assignRoles, checkWinCondition, validatePlayerCount, validateRoleDistribution } from './gameLogic';
+
+// Re-export functions
+export { assignRoles, checkWinCondition, validatePlayerCount, validateRoleDistribution }; 
